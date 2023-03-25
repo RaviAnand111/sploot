d0d7c94d4d94b254003b2f24f835c9fe634dd261
ravif22labs <ravi@f22labs.com>
2023-03-25 17:41:06 +0530

    fixed logout and user data in navbar after login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ProfileIcon } from "../asset";
-import { removeProfile } from "../features/profile/profileSlice";
+import { addProfile, removeProfile } from "../features/profile/profileSlice";
 import { getUserDetail } from "../Helper/Auth";
 import "./Navbar.css";
 
 function Navbar() {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState();
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
 
   const data = useSelector((state) => state?.userProfile?.profile?.data);
   useEffect(() => {
@@ -19,9 +18,10 @@ function Navbar() {
     };
     if (!data) getData();
     else {
+      dispatch(addProfile(data));
       setUserData(data);
     }
-  }, []);
+  }, [data]);
 
   function handleLogOut() {
     localStorage.removeItem("userToken");
@@ -34,7 +34,7 @@ function Navbar() {
       <a href="/">
         <img src="/sploot-logo.jpg" alt="logo" width="80px" height="30px"></img>
       </a>
-
+      {console.log(userData, "usr")}
       {userData && (
         <div className="user">
           <button onClick={handleLogOut} className="logout-btn">
